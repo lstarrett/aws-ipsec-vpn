58f9b72aadb4fe7f33464cd339e93daa0995978a
Lucas Starrett <lucas.starrett@analog.com>
2018-12-05 00:28:36 -0500

    Initial commit of EC2 IPSec VPN Tool

---
+++ README.md
@@ -0,0 +1,41 @@
+# EC2 IPSec VPN Tool
+This script quickly and painlessly launches an AWS EC2 Instance running Ubuntu
+16.04LTS and installs and launches a libreswan IPSec VPN server that can be
+used to tunnel traffic through the AWS geographic region in which the instance
+resides. This script is intended to be single click and dead simple to use.
+
+### Dependencies
+1. Bash
+2. Required packages: 'awscli', 'perl', 'jq'
+3. Optional packages: 'pbcopy'
+
+
+### Pre-requisites
+1. Must have an AWS account with provisioned CLI access profile and keys.
+2. Must have a Security Group created in your AWS account in every region you
+   will target with this tool named 'IPSECVPN' with the following inbound rules
+   set
+	1. Custom UDP port 500 (ISAKMP) - Allow from any source
+	2. Custom UDP port 1701 (L2TP) - Allow from any source
+	3. Custom UDP port 4500 (ESP) - Allow from any source
+
+### Installation
+1. Clone this repository to a capable local machine and cd into the project's root
+   directory
+2. Edit configuration file setting proper AWS region, AWS profile, and desired
+   IPSec VPN PSK, username, and password
+
+### Use
+1. Run `./vpn.sh start` to launch VPN instance with configuration settings in
+   'config.json'
+2. After launching, check status of VPN instance with `./vpn.sh status` (NOTE:
+   it may take a minute or two for any status to be available)
+3. When VPN instance is running, retrieve IP address with `./vpn.sh ip`
+4. Configure a new VPN connection profile on your local device with the
+   following settings
+	1. VPN type: L2TP over IPSec
+	2. VPN shared secret or PSK: PSK in 'config.json'
+	3. VPN username: username in 'config.json'
+	4. VPN password: password in 'config.json'
+5. Connect to VPN, and test connection by checking public IP of local device
+   (via Google search query 'my ip')
---
+++ vpn.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+#
+# Author(s): Lucas Starrett (lucas.c.starrett@gmail.com)
+#
+# This script launches an Ubuntu 16.04 LTS t2.micro EC2 instance and installs a libreswan IPSec
+# VPN server on it that may be used to tunnel traffic through geographic region in which EC2 instance
+# resides.
+#
+# This script is intended to be single click, dead simple use.
+#
+
+# load config variables
+CONFIG=`cat config.json`
+
+VPN_CREATE_SCRIPT=`echo $CONFIG | jq -r '.vpn_create_script_path'`
+REGION=`echo $CONFIG | jq -r '.aws_region'`
+PROFILE=`echo $CONFIG | jq -r '.aws_profile'`
+USERNAME=`echo $CONFIG | jq -r '.ipsec_username'`
+PASSWORD=`echo $CONFIG | jq -r '.ipsec_password'`
+PSK=`echo $CONFIG | jq -r '.ipsec_psk'`
+
+# check for dependencies
+command -v aws >/dev/null 2>&1 || { echo >&2 "Dependency 'awscli' required, please install before continuing. Aborting."; exit 1; }
+command -v perl >/dev/null 2>&1 || { echo >&2 "Dependency 'perl' required, please install before continuing. Aborting."; exit 1; }
+command -v jq >/dev/null 2>&1 || { echo >&2 "Dependency 'jq' required, please install before continuing. Aborting."; exit 1; }
+command -v pbcopy >/dev/null 2>&1 || { echo >&2 "Dependency 'pbcopy' required, please install before continuing, or comment out line in 'vpn.sh'. Aborting."; exit 1; }
+
+# parse command
+case "$1" in
+    start)
+        # replace username, password, and PSK variables in VPN create script
+        cp $VPN_CREATE_SCRIPT temp
+        perl -pi -e "s/IPSEC-USERNAME-PLACEHOLDER/$USERNAME/g" temp
+        perl -pi -e "s/IPSEC-PASSWORD-PLACEHOLDER/$PASSWORD/g" temp
+        perl -pi -e "s/IPSEC-PSK-PLACEHOLDER/$PSK/g" temp
+
+        # choose the right AMI based on region
+        case "$REGION" in
+            us-east-1)
+                AMI=`echo $CONFIG | jq -r '.ubuntu_us_east_1'`
+                ;;
+            eu-west-1)
+                AMI=`echo $CONFIG | jq -r '.ubuntu_eu_west_1'`
+                ;;
+            *)
+                echo "Unsupported region. Use 'us-east-1' or 'eu-west-1', or add support for more AMIs to the script. Exiting."
+                exit 1
+                ;;
+        esac
+
+        # start the VPN instance and pass it the create script as userdata
+        echo "Starting IPsec VPN on AWS EC2..."
+        aws ec2 run-instances \
+                --region $REGION \
+                --image-id $AMI \
+                --count 1 \
+                --instance-type t2.micro \
+                --key-name lucas \
+                --security-groups IPSECVPN \
+                --user-data file://temp \
+                --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ipsecvpn}]' \
+                > vpn-instance-config
+
+        # clean up
+        rm temp
+        echo "Done."
+        ;;
+
+    ip)
+        echo "Retrieving AWS VPN Instance IP Address..."
+        INSTANCE_IP=`aws ec2 describe-instances \
+                --region $REGION \
+                --query 'Reservations[*].Instances[*].[PublicIpAddress]' \
+                --filters "Name=tag-value,Values=ipsecvpn" \
+                --output text`
+        echo -n $INSTANCE_IP | pbcopy
+        echo "Instance IP $INSTANCE_IP copied to clipboard"
+        echo
+        ;;
+
+    status)
+        echo "Retrieving AWS VPN Instance Status..."
+        INSTANCE_ID=`cat vpn-instance-config | jq -r '.Instances | .[] | .InstanceId'`
+        aws ec2 describe-instance-status \
+                --region $REGION \
+                --instance-id $INSTANCE_ID
+        ;;
+
+    stop)
+        echo "Stopping and Terminating AWS VPN Instance..."
+        INSTANCE_ID=`cat vpn-instance-config | jq -r '.Instances | .[] | .InstanceId'`
+        aws ec2 terminate-instances \
+                --region $REGION \
+                --instance-ids $INSTANCE_ID
+
+        # clean up
+        rm vpn-instance-config
+        echo "Done."
+        ;;
+
+    *)
+        echo "Usage: $0 {start|ip|status|stop}"
+        exit 1
+esac
+
+exit $?
+
